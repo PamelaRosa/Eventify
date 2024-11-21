fb7024be05f11f223f796d37a5dbd0bbbba34ad8
PamelaRosa <pa.rosa4@gmail.com>
2024-11-20 23:12:17 -0300

    Criação tabela de eventos e implementação de filtragem por tema e localização

--- Frontend/EventifyApp/src/app/app.module.ts
+++ Frontend/EventifyApp/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventsComponent } from './events/events.component';
 import { SpeakersComponent } from './speakers/speakers.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 
 import { CollapseModule } from 'ngx-bootstrap/collapse';
@@ -23,7 +24,8 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    CollapseModule.forRoot()
+    CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Frontend/EventifyApp/src/app/events/events.component.ts
+++ Frontend/EventifyApp/src/app/events/events.component.ts
@@ -7,7 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./events.component.scss'],
 })
 export class EventsComponent implements OnInit {
-  public events: any;
+  public events: any = [];
+  public filteredEvents: any = [];
+  widthImgThumbnail = 50;
+  private _filterList: string = '';
+
+  public get filterList(): string {
+    return this._filterList;
+  }
+
+  public set filterList(value: string) {
+    this._filterList = value;
+    this.filteredEvents = this.filterList
+      ? this.filterEvents(this.filterList)
+      : this.events;
+  }
+
+  filterEvents(filterBy: string): any {
+    filterBy = filterBy.toLocaleLowerCase().trim();
+    return this.events.filter(
+      (event: any) =>
+        event.theme.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+        event.location.toLocaleLowerCase().indexOf(filterBy) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -17,8 +40,11 @@ export class EventsComponent implements OnInit {
 
   public getEvents(): void {
     this.http.get('https://localhost:5001/api/events').subscribe(
-      response => this.events = response,
-      error => console.error('Error retrieving events:', error),
+      (response) => {
+        this.events = response;
+        this.filteredEvents = this.events;
+      },
+      (error) => console.error('Error retrieving events:', error)
     );
   }
 }
